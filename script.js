1beeacc6707de9d7add2fe8b276fdf1e47eff877
Ashton Taylor <ashtonwtaylor@gmail.com>
2026-07-20 10:10:27 -0500

    Fetched geoJSON layer in script, countries are now clickable and log the name of the clicked country to the console

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+fetch('assets/countries.geojson')
+    .then(res => res.json())
+    .then(data => {
+        L.geoJSON(data, {
+            onEachFeature: (feature, layer) => {
+                layer.on('click', () => {
+                    console.log(feature.properties.name, feature.properties.iso_a2);
+                });
+            }
+        }).addTo(map);
+    });
